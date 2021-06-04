0069ca601f657a6f4c90b86a4e3eebbf8c48305c
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-06-03 21:35:20 -0500

    Creando controlador con la acción handler listar

--- src/main/java/com/ideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/ideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -3,17 +3,21 @@ package com.ideas.springboot.app.models.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ideas.springboot.app.models.entity.Cliente;
 
-@Repository
+@Repository("clienteDaoJPA")
 public class ClienteDaoImpl implements IClienteDao {
 
+	@PersistenceContext
 	private EntityManager em;
 	
 	@SuppressWarnings("unchecked")
+	@Transactional(readOnly=true)
 	@Override
 	public List<Cliente> findAll() {
 		// TODO Auto-generated method stub
